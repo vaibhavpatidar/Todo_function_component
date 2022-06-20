9d824e1bb922cd7e12817e470d8d90a4452c9507
Vaibhavpatidar <Vaibhavpatidar89@gmail.com>
2022-06-20 12:09:38 +0530

    Todo List

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './containers/Login'
+import Registration from './containers/Registration'
+import TodoList from './containers/Todo'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/todolist" element={<TodoList />} />
+        </Routes>
+
+      </BrowserRouter>
+   
   );
 }
 
